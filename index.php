067fe9359c226b244961e93ac1b43ac496937701
smm2019 <46477961+smm2019@users.noreply.github.com>
2024-01-27 15:22:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,38 @@
 <?php
-date_default_timezone_set("Asia/Shanghai");
-$channel = empty($_GET['id']) ? "cctv16hd4k/15000000" : trim($_GET['id']);
-$stream = "http://223.111.117.11/liveplay-kk.rtxapp.com/live/program/live/{$channel}/";
-$timestamp = substr(time(), 0, 9) - 7;
-$current = "#EXTM3U" . PHP_EOL;
-$current .= "#EXT-X-VERSION:3" . PHP_EOL;
-$current .= "#EXT-X-TARGETDURATION:3" . PHP_EOL;
-$current .= "#EXT-X-MEDIA-SEQUENCE:{$timestamp}" . PHP_EOL;
-for ($i = 0; $i < 3; $i++) {
-    $timematch = $timestamp . '0';
-    $timefirst = date('YmdH', $timematch);
-    $current .= "#EXTINF:3," . PHP_EOL;
-    $current .= $stream . $timefirst . "/" . $timestamp . ".ts" . PHP_EOL;
-    $timestamp = $timestamp + 1;
-}
-header("Content-Type: application/vnd.apple.mpegurl");
-header("Content-Disposition: attachment; filename=index.m3u8");
-echo $current;
+$id = isset($_GET['id'])?$_GET['id']:'gxpd';
+$n = array(
+'hnws' => 145,//河南卫视
+'hnds' => 141,//河南都市
+'hnms' => 146,//河南民生
+'hmfz' => 147,//河南法治
+'hndsj' => 148,//河南电视剧
+'hnxw' => 149,//河南新闻
+'htgw' => 150,//欢腾**
+'hngg' => 151,//河南公共
+'hnxc' => 152,//河南乡村
+'hngj' => 153,//河南国际
+'hnly' => 154,//河南梨园
+'wwbk' => 155,//文物宝库
+'wspd' => 156,//武术世界
+'jczy' => 157,//睛彩中原
+'ydxj' => 163,//移动戏曲
+'xsj' => 183,//象视界
+'gxpd' => 194,// 国学频道
+);
+$t = time();
+$sign = hash('sha256','6ca114a836ac7d73'.$t);
+$ch = curl_init('https://pubmod.hntv.tv/program/getAuth/live/class/program/11');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['timestamp:'.$t, 'sign:'.$sign,]);
+$d = curl_exec($ch);
+curl_close($ch);
+$j = json_decode($d);
+for($i=0;$i<count($j);$i++){
+   if($n[$id] == $j[$i] -> cid)
+       $playurl = $j[$i] -> video_streams[0];
+       }
+header('Location:'.$playurl);
+//echo $playurl;
+?>
